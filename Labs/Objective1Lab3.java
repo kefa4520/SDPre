62216ee6d11cd8fe236931c4eb8175bfa51459f8
Iryna <kefa_tretinik@icloud.com>
2020-01-15 16:24:11 -0700

    complete objective1lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3{
+  public static void main(String[] args){
+    System.out.println( "Name: Iryna Tretynyk" );
+    System.out.println( "Age: 30" );
+    System.out.println( "State: New York" );
+  }
+}
