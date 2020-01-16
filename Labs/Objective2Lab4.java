cf693f84f804397d5078f6fb6e6c6bda42507547
Iryna <kefa_tretinik@icloud.com>
2020-01-16 08:38:24 -0700

    complete objective2lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,11 @@
+public class Objective2Lab4 {
+  public static void main(String[] args ) {
+    double num1, num2, num3, average;
+
+    num1 = 5;
+    num2 = 6;
+    num3 = 7;
+    average = (num1 + num2 + num3)/3;
+    System.out.println ( average );
+  }
+}
