183dccdbf11a60e8916fd9fecd0a61f0caddfce8
Iryna <kefa_tretinik@icloud.com>
2020-01-16 07:50:20 -0700

    complete objecive2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main( String[] args) {
+    String name = "Iryna Tretynyk";
+    int age = 30;
+    String state = "New York";
+
+    System.out.println( "Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
