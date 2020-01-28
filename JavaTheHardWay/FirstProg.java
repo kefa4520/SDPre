4dc68c87aaaa430219acc0e8a68525cf5070b541
Iryna <kefa_tretinik@icloud.com>
2020-01-27 17:04:02 -0700

    finish ex 1

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main( String[] args ) {
-    System.out.println( "I, Iryna Tretynyk, am determined to learn how to code." );
-    System.out.println( "Today's date is Monday, January 27, 2015." );
+    System.out.println( "I am determined to learn how to code." );
+    System.out.println(  "Today's date is" );
   }
 }
