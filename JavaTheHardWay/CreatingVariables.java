06c3014ebf20ae753ee32f1ca7e85a8e1df2fb61
Iryna <kefa_tretinik@icloud.com>
2020-02-08 18:49:09 -0700

    finish jthw ex 10

--- JavaTheHardWay/CreatingVariables.java
+++ JavaTheHardWay/CreatingVariables.java
@@ -1,20 +1,26 @@
 public class CreatingVariables {
   public static void main( String[] args ){
-    int x, y, age;
-    double seconds, e, checking;
-    String firstName, lastName, title;
+    //study drill: Add four more variables to the program: another integer, another double, and two Strings.
+    //Name them whatever you want. Give them values. Print them out.
+    int x, y, year, age;
+    double seconds, e, checking, weight;
+    String firstName, lastName, title, course, schoolName;
 
     x = 10;
     y = 400;
-    age = 39;
+    year = 2019;
+    age = 30;
 
     seconds = 4.71;
     e = 2.71828182845904523536;
     checking = 1.89;
+    weight = 144.5;
 
     firstName = "Iryna";
     lastName = "Tretynyk";
     title = "Ms.";
+    course = "Java Coding";
+    schoolName = "Skill Distillery";
 
     System.out.println( "The variable x contains " + x );
     System.out.println( "The value " + y + " is stored in the variable y." );
@@ -22,5 +28,7 @@ public class CreatingVariables {
     System.out.println( "A favorite irrational # is Euler's number: " + e );
     System.out.println( "Hopefully you have more than $" + checking + "!" );
     System.out.println( "My name's " + title + " " + firstName + lastName );
+    System.out.println( "In " + year + " I turned " + age + " and my weight was " + weight);
+    System.out.print( "I study " + course + " at " + schoolName);
   }
 }
