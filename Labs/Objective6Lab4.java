a47af64dcbcc7b408849f99eb8f219095c9b77dc
Iryna <kefa_tretinik@icloud.com>
2020-03-05 20:09:56 -0700

    complete objective6lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,12 @@
+public class Objective6Lab4 {
+  public static void main( String[] args ) {
+    int count = 1;
+    int sum = 0;
+
+    while (count <=20 ) {
+      sum = count + sum;
+      count++;
+}
+      System.out.print(sum);
+    }
+  }
