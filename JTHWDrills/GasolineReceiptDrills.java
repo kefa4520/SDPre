1ae8fefc380accffc83568c7c2d4a627eaa35e70
Iryna <kefa_tretinik@icloud.com>
2020-03-11 15:06:53 -0600

    ex 2 drills

---
+++ JTHWDrills/GasolineReceiptDrills.java
@@ -0,0 +1,16 @@
+public class GasolineReceiptDrills {
+  public static void main(String[] args ){
+    System.out.println( "+------------------------+" );
+    System.out.println( "|                        |" );
+    System.out.println( "|      CORNER STORE      |" );
+    System.out.println( "|                        |" );
+    System.out.println( "|  2020-03-11  14:56PM   |" );
+    System.out.println( "|                        |" );
+    System.out.println( "|  Gallons:      12.520  |" );
+    System.out.println( "|  Price/gallon  $ 1.99  |" );
+    System.out.println( "|                        |" );
+    System.out.println( "|  Fuel total:  $ 24.91  |" );
+    System.out.println( "|                        |" );
+    System.out.println( "+------------------------+" );
+  }
+}
